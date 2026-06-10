986b7fbeeca2b5a2589213c69f9c1cdebd851826
anti-society-projectx <anti.society.projectx@gmail.com>
2026-06-11 01:45:24 +0300

    feat: добавил классы с короткими ссылками и статистикой об переходах по ссылкам

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,7 +1,12 @@
-from sqlalchemy import BigInteger
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
 
-from backend.models.base import Base
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.models import Base
+
+if TYPE_CHECKING:
+    from backend.models import Link
 
 
 class User(Base):
@@ -11,3 +16,10 @@ class User(Base):
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
 
     username: Mapped[str | None] = mapped_column(unique=True, index=True)
+
+    links: Mapped[list["Link"]] = relationship(
+        "Link",
+        back_populates="user",
+        cascade="all, delete-orphan",
+        lazy="selectin"
+    )
